2af180728bfba49e77afba3a67bb55a35372d829
Pan Cheng <pchengeducation@hotmail.com>
2018-08-24 21:24:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,23 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+        get '/' do
+          erb :'views/root'
+        end
 
-  end
+        get '/new' do
+          erb :'view/pirates/new'
+        end
+
+        post '/pirates' do
+          @pirate = Pirate.new(params[:pirate])
+
+          params[:pirate][:ships].each do |details|
+            Ship.new(details)
+          end
+          @ships = Ship.all
+
+          erb :'views/pirates/show'
+        end
+      end
 end
